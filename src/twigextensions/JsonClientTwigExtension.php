c9395a80f5f60439290ec27b8fa04dec807024c4
Jay Collett <jay@jay-collett.com>
2018-08-23 09:56:25 +0100

    Lets encrypt SSL certificates fix
    Adding one line to allow data to be transferred between sites using lets encrypt ssl certificates.

--- src/twigextensions/JsonClientTwigExtension.php
+++ src/twigextensions/JsonClientTwigExtension.php
@@ -61,6 +61,7 @@ class JsonClientTwigExtension extends Twig_Extension
 			$ch = curl_init();
 			curl_setopt($ch, CURLOPT_URL, $url);
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 			$store = curl_exec($ch);
 			curl_close($ch);
 
